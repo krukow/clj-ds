65ce5965859c054470ad2d9c9d99a66a847d8193
Stanislas Nanchen <stanislas.nanchen@gmail.com>
2013-09-04 11:59:05 +0200

    Simplify Names in Persistents.

--- src/main/java/com/trifork/clj_ds/Persistents.java
+++ src/main/java/com/trifork/clj_ds/Persistents.java
@@ -1,6 +1,5 @@
 package com.trifork.clj_ds;
 
-import java.util.List;
 import java.util.Map;
 
 import com.trifork.clj_lang.PersistentArrayMap;
@@ -12,87 +11,103 @@ import com.trifork.clj_lang.PersistentTreeSet;
 
 public final class Persistents {
 	
-	public static <E> PersistentVector<E> emptyVector() {
+	public static <E> PersistentVector<E> vector() {
 		return com.trifork.clj_lang.PersistentVector.emptyVector();
 	}
+	
+	public static <E> PersistentVector<E> vector(E item) {
+		return Persistents.<E>vector().cons(item);
+	}
 
-	public static <E> PersistentVector<E> newVector(E... items) {
+	public static <E> PersistentVector<E> vector(E... items) {
 		return com.trifork.clj_lang.PersistentVector.create(items);
 	}
 	
-	public static <E> PersistentVector<E> newVector(Iterable<? extends E> items) {
+	public static <E> PersistentVector<E> vector(Iterable<? extends E> items) {
 		return com.trifork.clj_lang.PersistentVector.create(items);
 	}
 	
-	public static <E> PersistentSet<E> emptyHashSet() {
+	public static <E> PersistentSet<E> hashSet() {
 		return PersistentHashSet.emptySet();
 	}
 	
-	public static <E> PersistentSet<E> newHashSet(E... items) {
+	public static <E> PersistentSet<E> hashSet(E item) {
+		return Persistents.<E>hashSet().cons(item);
+	}
+	
+	public static <E> PersistentSet<E> hashSet(E... items) {
 		return PersistentHashSet.create(items);
 	}
 	
-	public static <E> PersistentSet<E> newHashSet(Iterable<? extends E> items) {
+	public static <E> PersistentSet<E> hashSet(Iterable<? extends E> items) {
 		return PersistentHashSet.create(items);
 	}
 	
-	public static <K, V> PersistentMap<K, V> emptyHashMap() {
+	public static <K, V> PersistentMap<K, V> hashMap() {
 		return PersistentHashMap.emptyMap();
 	}
 	
-	public static <K, V> PersistentMap<K, V> newHashMap(Object... keyValues) {
+	public static <K, V> PersistentMap<K, V> hashMap(K key, V val) {
+		return Persistents.<K, V>hashMap().assoc(key, val);
+	}
+	
+	public static <K, V> PersistentMap<K, V> hashMap(Object... keyValues) {
 		return PersistentHashMap.create(keyValues);
 	}
 	
-	public static <K, V> PersistentMap<K, V> newHashMap(Map<? extends K, ? extends V> init) {
+	public static <K, V> PersistentMap<K, V> hashMap(Map<? extends K, ? extends V> init) {
 		return PersistentHashMap.create(init);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static <K, V> PersistentSortedMap<K, V> emptyTreeMap() {
+	public static <K, V> PersistentSortedMap<K, V> treeMap() {
 		return PersistentTreeMap.EMPTY;
 	}
 	
-	public static <K, V> PersistentSortedMap<K, V> newTreeMap(Map<? extends K, ? extends V> init) {
+	public static <K, V> PersistentSortedMap<K, V> treeMap(K key, V val) {
+		return Persistents.<K, V>treeMap().assoc(key, val);
+	}
+	
+	public static <K, V> PersistentSortedMap<K, V> treeMap(Map<? extends K, ? extends V> init) {
 		return PersistentTreeMap.create(init);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static <E> PersistentSortedSet<E> emptyTreeSet() {
+	public static <E> PersistentSortedSet<E> treeSet() {
 		return PersistentTreeSet.EMPTY;
 	}
 	
 	/*
-	public static <E> PersistentSortedSet<E> newTreeSet(E... items) {
+	public static <E> PersistentSortedSet<E> treeSet(E... items) {
 		return PersistentTreeSet.create(items);
 	}
 	
-	public static <E> PersistentSortedSet<E> newTreeSet(Iterable<? extends E> items) {
+	public static <E> PersistentSortedSet<E> treeSet(Iterable<? extends E> items) {
 		return PersistentTreeSet.create(items);
 	}*/
 	
 	@SuppressWarnings("unchecked")
-	public static <K, V> PersistentMap<K, V> emptyArrayMap() {
+	public static <K, V> PersistentMap<K, V> arrayMap() {
 		return PersistentArrayMap.EMPTY;
 	}
 	
-	public static <K, V> PersistentMap<K, V> newArrayMap(Map<? extends K, ? extends V> init) {
+	public static <K, V> PersistentMap<K, V> arrayMap(Map<? extends K, ? extends V> init) {
 		return PersistentArrayMap.create(init);
 	}
 	
-	public static <E> PersistentList<E> emptyLinkedList() {
+	public static <E> PersistentList<E> linkedList() {
 		return com.trifork.clj_lang.PersistentList.emptyList();
 	}
 	
-	public static <E> PersistentList<E> newLinkedList(E... items) {
+	public static <E> PersistentList<E> linkedList(E item) {
+		return Persistents.<E>linkedList().cons(item);
+	}
+	
+	public static <E> PersistentList<E> linkedList(E... items) {
 		return com.trifork.clj_lang.PersistentList.create(items);
 	}
 	
-	public static <E> PersistentList<E> newLinkedList(Iterable<? extends E> items) {
-		return com.trifork.clj_lang.PersistentList.create(items);
-	}
-	
-	public static <E> PersistentList<E> newLinkedList(List<? extends E> items) {
+	public static <E> PersistentList<E> linkedList(Iterable<? extends E> items) {
 		return com.trifork.clj_lang.PersistentList.create(items);
 	}
 	
